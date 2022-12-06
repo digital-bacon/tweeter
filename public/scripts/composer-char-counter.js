d3a16ddc36561f33464b665430cfe743d5375f41
Mark Bieganek <mark.bieganek@telus.com>
2022-12-06 15:57:26 -0700

    Reflect key press count in output counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   const tweetText = $('#tweet-text');
-  $(tweetText).on('keypress', function() {
-    console.log(this.textLength);
+  const tweetCounter = $('output.counter');
+  $(tweetText).on('keydown', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+    const tweetLength = this.value.length;
+    $(tweetCounter).text(tweetLength);
   })
 });
