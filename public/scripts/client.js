246348bfce6962ea925b342ad4c3130ba237072a
Mark Bieganek <mark.bieganek@telus.com>
2022-12-07 15:55:56 -0700

    Create renterTweets() function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -43,6 +43,39 @@ $(document).ready(function() {
     return output;
   }
 
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+  const renderTweets = function(tweets, outputElement) {
+    $(outputElement).empty();
+    $.each(tweets, (index, tweetObject) => $('#tweets-container')
+      .append(createTweetElement(tweetObject))
+    );
+  };
+
   const createTweetElement  = (tweetObject) => {
     const $article = $('<article>').addClass('tweet');
     const $header = $('<header>');
@@ -75,6 +108,7 @@ $(document).ready(function() {
       'src': tweetObject.user.avatars,
       'alt': `Profile image for ${tweetObject.user.name}`
     });
+
     $($contentText).text(tweetObject.content.text);
     const tweetAgeInDays = calculateDateDiff('days', Date.now(), new Date(tweetObject.created_at));
     const ageOutput = tweetAgeInDays <= 1 ? 'Today' : `${tweetAgeInDays} days ago`;
@@ -85,38 +119,25 @@ $(document).ready(function() {
         'title': 'Report tweet',
       })
       .html(`<i class='fa-solid fa-flag'></i>`);
+  
     $($actionRetweet)
       .attr({
         'href': '#',
         'title': 'Retweet',
       })
       .html(`<i class='fa-solid fa-retweet'></i>`);
+
     $($actionSaveTweet)
       .attr({
         'href': '#',
         'title': 'Save Tweet',
       })
       .html(`<i class='fa-solid fa-heart'></i>`);
+
     return $article;
   }
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
+ renderTweets(data, '#tweets-container');
 
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); 
 });
 
