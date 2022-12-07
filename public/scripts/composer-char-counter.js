efa3ac8e483bbbb3ef0eda7828e1f97329b58741
Mark Bieganek <mark.bieganek@telus.com>
2022-12-07 09:24:22 -0700

    Abstract set counter text + tweet length check to functions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,20 +3,30 @@ $(document).ready(function() {
     if (event.key === 'Enter') {
       event.preventDefault();
     }
+  };
+
+  const getCounterText = (maxTweetLength, currentTweetLength) => {
+    const counterValue = maxTweetLength - currentTweetLength;
+    return counterValue;
   }
 
-  const tweetTextInput = $('#tweet-text');
-  const tweetTextParent = $(tweetTextInput).parent();
-  const tweetCounter = $(tweetTextParent).find('.counter');
+  const isTweetLengthExceeded = (maxTweetLength, currentTweetLength) => {
+    isExceeded = (maxTweetLength - currentTweetLength) < 0;
+    return isExceeded;
+  }
+
+  const tweetInputElement = $('#tweet-text');
+  const tweetInputParent = $(tweetInputElement).parent();
+  const counterElement = $(tweetInputParent).find('.counter');
   const maxTweetLength = 149;
-  $(tweetCounter).text(maxTweetLength);
-  $(tweetTextInput).on('keydown', function(event) {
+  $(counterElement).text(maxTweetLength);
+  $(tweetInputElement).on('keydown', function(event) {
     handleEnterKey(event);
     const currentLength = this.value.length;
-    const counterValue = maxTweetLength - currentLength;
-    $(tweetCounter).text(counterValue);
-    if (counterValue < 0) {
-      $(tweetCounter).removeClass('danger').addClass('danger');
+    const counterText = getCounterText(maxTweetLength, currentLength);
+    $(counterElement).text(counterText);
+    if (isTweetLengthExceeded(maxTweetLength, currentLength)) {
+      $(counterElement).removeClass('danger').addClass('danger');
     }
 
   })
