4298a0e633369a8947156f05a10a81c5470fc211
Mark Bieganek <mark.bieganek@telus.com>
2022-12-07 09:03:03 -0700

    Abstract enter key handling to a function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
+  const handleEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  }
+
   const tweetTextInput = $('#tweet-text');
   const tweetTextParent = $(tweetTextInput).parent();
   const tweetCounter = $(tweetTextParent).find('.counter');
   const maxTweetLength = 149;
   $(tweetCounter).text(maxTweetLength);
   $(tweetTextInput).on('keydown', function(event) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-    }
-
+    handleEnterKey(event);
     const currentLength = this.value.length;
     const counterValue = maxTweetLength - currentLength;
     $(tweetCounter).text(counterValue);
