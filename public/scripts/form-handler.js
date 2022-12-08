706527385b31839fbd9e3fad1697a62e1b55da12
Mark Bieganek <mark.bieganek@telus.com>
2022-12-07 18:03:00 -0700

    Prevent default form post, post via jQuery instead

--- public/scripts/form-handler.js
+++ public/scripts/form-handler.js
@@ -1,3 +1,12 @@
 $(() => {
   
+  const $newTweetForm = $('#new-tweet');
+  $newTweetForm.on('submit', (event) => {
+    event.preventDefault();
+    const tweetData = $newTweetForm.serialize();
+    $.post('/', tweetData, (response) => {
+      console.log(response);
+    });
+  });
+  
 });
