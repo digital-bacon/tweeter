420ca5b95abcb1f420ededd5e59055621d79c3bc
Mark Bieganek <mark.bieganek@telus.com>
2022-12-09 08:33:18 -0700

    Refactor for modularity and abstraction

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
-  const $tweetInputElement = $('#tweet-text');
-  const $counterElement = $('.counter').text(MAX_TWEET_LENGTH);
-  $($tweetInputElement).on('keyup', function(event) {
-    const currentTweetLength = this.value.length;
-    const counterText = MAX_TWEET_LENGTH - currentTweetLength;
-    $counterElement.text(counterText);
-    const isLengthExceeded = counterText < 0;
-    if (isLengthExceeded === true) {
-      $counterElement.removeClass('danger').addClass('danger');
+  /**
+   * Function that calculates total tweet length remaining
+   * @param {string | Object} inputSelector - The selector of the counter
+   * element. Accepts any valid jQuery selector
+   * @param {number} maxLength - The maximum number of characters
+   * allowed for this tweet
+   * @returns {number} The total tweet length remaining
+   */
+  const getRemainingTweetLength = (inputSelector, maxLength) => {
+    const currentLength = $(inputSelector).val().trim().length;
+    const remainingLength = maxLength - currentLength;
+    return remainingLength;
+  };
+  
+  /**
+   * Function that toggles a .danger class on the tweet counter
+   * if the current tweet exceeds the remaining tweet length
+   * @param {string | Object} counterSelector - The selector of the counter
+   * element. Accepts any valid jQuery selector
+   * @param {number} remainingTweetLength - The number of characters
+   * remaining of allowed maximum for this tweet
+   * @returns {undefined}
+   */
+  const setCounterClass = (counterSelector, remainingTweetLength) => {
+    const $counterElement = $(counterSelector);
+    if (remainingTweetLength < 0) {
+      $($counterElement).removeClass('danger').addClass('danger');
       return;
     }
 
     $counterElement.removeClass('danger');
+  };
+
+  const $tweetInputElement = $('#tweet-text');
+  const $counterElement = $('.counter').text(MAX_TWEET_LENGTH);
+  $($tweetInputElement).on('keyup', () => {
+    const counterValue = getRemainingTweetLength($tweetInputElement, MAX_TWEET_LENGTH);
+    $($counterElement).text(counterValue);
+    setCounterClass($counterElement, counterValue);
   });
 
 });
